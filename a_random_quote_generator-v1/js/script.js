d1e57e05935c1bf6b34e70ab26ee320d0fdf6289
browe83 <63806760+browe83@users.noreply.github.com>
2020-04-16 14:51:04 -0400

    Created quotes array

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -10,7 +10,50 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
+let quotes = [
+  {quote: `A goal without a plan is a wish.` ,
+  source: `Antoine de Saint-Exupéry`,
+  citation: null,
+  year: null,
+  nationality: `French`
+},
 
+  {quote: `Every result that you desire is preceded by a process that is required to produce the result. When you define YOUR process and commit to it for an extended period of time, the results take care of themselves.`,
+  source: `Hal Elrod`,
+  citation: `The Miracle Morning for Salespeople`,
+  year: 2015,
+  nationality: `American`
+},
+
+  {quote: `If you want to increase your success rate, double your failure rate.` ,
+  source: `Thomas Watson`,
+  citation: null,
+  year: null,
+  nationality: `American`
+},
+
+  {quote: `The impediment to action advances action...  What stands in the way becomes the way.` ,
+  source: `Marcus Aurelius`,
+  citation: `Meditations`,
+  year: `161 - 180 CE`, 
+  nationality: `Roman`
+},
+
+  {quote: `Being busy is most often used as a guise for avoiding the few critically important but uncomfortable actions.` ,
+  source: `Timothy Ferriss`,
+  citation: `The 4-Hour Workweek`,
+  year: 2007,
+  nationality: `American`
+},
+
+  {quote: `Nothing in the world is worth having or worth doing unless it means effort, pain, difficulty… I have never in my life envied a human being who led an easy life.` ,
+  source: `Theodore Roosevelt`,
+  citation: null,
+  year: null,
+  nationality: `American`
+}
+
+] 
 
 
 /***
